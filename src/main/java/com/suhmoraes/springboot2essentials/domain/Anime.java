1758ff8bb5bfe751063cc7f617200a9bf4637c0f
SuhMoraes <suh.alvesmoraes@gmail.com>
2021-10-17 22:03:36 -0300

    Inserção de dependências e configuração do BD

--- src/main/java/com/suhmoraes/springboot2essentials/domain/Anime.java
+++ src/main/java/com/suhmoraes/springboot2essentials/domain/Anime.java
@@ -1,9 +1,16 @@
 package com.suhmoraes.springboot2essentials.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Objects;
 
+@Entity
 public class Anime {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
